f6144821002691974923b86eaf1f465716e403f3
kusemererwaedson <kusemererwaedson2000@gmail.com>
2024-02-23 13:27:21 +0300

    ecom9

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+// require __DIR__.'/auth.php';  
+
+
+Route::prefix('/admin')->namespace('App\Http\Controllers\Admin')->group(function(){
+
+    // Admin Login Route 
+    Route::match(['get','post'],'login','AdminController@login');
+
+    // Admin Login Route 
+    Route::get('dashboard','AdminController@dashboard');
+});
+
